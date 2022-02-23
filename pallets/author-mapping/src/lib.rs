0d68bb60ca536ba6ef17589b8112dd8f9c02c568
Sankar boro <sankar.boro@yahoo.com>
2022-02-23 11:54:13 +0530

    temp deactive

--- pallets/author-mapping/src/lib.rs
+++ pallets/author-mapping/src/lib.rs
@@ -272,11 +272,12 @@ pub mod pallet {
 		}
 	}
 
-	impl<T: Config> AccountLookup<T::AuthorId, T::AccountId> for Pallet<T> {
-		fn lookup_account(author: &T::AuthorId) -> Option<T::AccountId> {
-			Self::account_id_of(author)
-		}
-	}
+	// axia:help
+	// impl<T: Config> AccountLookup<T::AuthorId, T::AccountId> for Pallet<T> {
+	// 	fn lookup_account(author: &T::AuthorId) -> Option<T::AccountId> {
+	// 		Self::account_id_of(author)
+	// 	}
+	// }
 
 	impl<T: Config> Pallet<T> {
 		/// A helper function to lookup the account id associated with the given author id. This is
